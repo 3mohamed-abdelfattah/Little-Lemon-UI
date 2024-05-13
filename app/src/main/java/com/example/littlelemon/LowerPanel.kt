ab08897da55c57571d527c647bbdc7d95df790b8
MohamedMohamed <m01126165016@icloud.com>
2024-05-13 16:57:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -0,0 +1,94 @@
+package com.example.littlelemon
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.littlelemon.ui.theme.LittleLemonColor
+
+@Composable
+fun LowerPanel(navController: NavHostController, dishes: List<Dish> = listOf()) {
+    Column {
+        WeeklySpecialCard()
+        LazyColumn {
+            itemsIndexed(dishes) { _, dish ->
+                MenuDish(navController, dish)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeeklySpecialCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.weekly_special),
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MenuDish(navController: NavHostController? = null, dish: Dish) {
+    Card(onClick = {
+        Log.d("AAA", "Click ${dish.id}")
+        navController?.navigate(DishDetails.route + "/${dish.id}")
+    }) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column {
+                Text(
+                    text = dish.name,
+                    style = MaterialTheme.typography.h2
+                )
+                Text(
+                    text = dish.description,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .padding(top = 5.dp, bottom = 5.dp)
+                        .fillMaxWidth(.75f)
+                )
+                Text(
+                    text = "$${dish.price}",
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+            Image(
+                painter = painterResource(id = dish.imageResource),
+                contentDescription = "",
+                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            )
+        }
+    }
+    Divider(
+        modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+        thickness = 1.dp,
+        color = LittleLemonColor.yellow
+    )
+}
---
+++ app/src/main/java/com/example/littlelemon/UpperPanel.kt
@@ -0,0 +1,76 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.littlelemon.ui.theme.LittleLemonColor
+
+@Composable
+fun UpperPanel() {
+    Column(
+        modifier = Modifier
+            .background(LittleLemonColor.green)
+            .padding(start = 12.dp, end = 12.dp, top = 16.dp, bottom = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.title),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = LittleLemonColor.yellow
+        )
+        Text(
+            text = stringResource(id = R.string.location),
+            fontSize = 24.sp,
+            color = LittleLemonColor.cloud
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(top = 20.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.description),
+                style = MaterialTheme.typography.body1,
+                color = LittleLemonColor.cloud,
+                modifier = Modifier
+                    .padding(bottom = 28.dp, end = 20.dp)
+                    .fillMaxWidth(0.6f)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.upperpanelimage),
+                contentDescription = "Upper Panel Image",
+                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                text = stringResource(id = R.string.order_button_text)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UpperPanelPreview() {
+    UpperPanel()
+}
